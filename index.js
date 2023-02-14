46f1bcccbd53c951fe66196d678dcbd72595fa9f
James Niyongira <lascasasjames85@gmail.com>
2023-02-13 22:24:24 -0500

    finished setting up app with one error

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 const express = require("express");
 const redis = require("redis");
 const app = express();
-const port = 3000;
-const client = redis.createClient();
+const port = 4001;
+const client = redis.createClient({
+  host: "redis-server",
+  port: 6379,
+});
+
+// client.on("error", (err) => console.log("Redis Client Error", err));
+(async () => await client.connect())();
 client.set("visits", 0);
 
 app.get("/", (req, res) => {
   client.get("visits", (err, visits) => {
+    if (err) {
+      console.log("//////////////////////////////////");
+      console.log(err);
+    }
     res.send("Number of visits is " + visits);
     client.set("visits", parseInt(visits) + 1);
   });
 });
+
+// app.get("/", (req, res) => {
+//     res.send("Hello World!");
+//     });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
